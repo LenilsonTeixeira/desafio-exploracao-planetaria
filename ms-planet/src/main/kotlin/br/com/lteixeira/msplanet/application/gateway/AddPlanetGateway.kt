b10815480af46f41a6983e90ff3c8224d0c143ed
Lenilson Teixeira <lenilsonts@gmail.com>
2022-09-10 19:36:13 -0300

    [ms-planet] - Adicionando addPlanetGateway

---
+++ ms-planet/src/main/kotlin/br/com/lteixeira/msplanet/application/gateway/AddPlanetGateway.kt
@@ -0,0 +1,9 @@
+package br.com.lteixeira.msplanet.application.gateway
+
+import br.com.lteixeira.msplanet.domain.AddPlanetDomain
+import br.com.lteixeira.msplanet.domain.AddedPlanetDomain
+
+interface AddPlanetGateway {
+
+    fun add(addPlanetDomain: AddPlanetDomain): AddedPlanetDomain
+}

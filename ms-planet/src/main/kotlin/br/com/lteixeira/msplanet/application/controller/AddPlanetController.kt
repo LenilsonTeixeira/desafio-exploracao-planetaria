78b0bbd5d402d9fbf48fd17021ef45590280bc5a
Lenilson Teixeira <lenilsonts@gmail.com>
2022-09-10 22:09:30 -0300

    [ms-planet] - Adicionando addPlanetController

---
+++ ms-planet/src/main/kotlin/br/com/lteixeira/msplanet/application/controller/AddPlanetController.kt
@@ -0,0 +1,20 @@
+package br.com.lteixeira.msplanet.application.controller
+
+import br.com.lteixeira.msplanet.api.AddPlanetApi
+import br.com.lteixeira.msplanet.api.model.AddPlanetRequest
+import br.com.lteixeira.msplanet.api.model.AddedPlanetResponse
+import br.com.lteixeira.msplanet.application.gateway.toAddPlanetDomain
+import br.com.lteixeira.msplanet.application.gateway.toAddedPlanetResponse
+import br.com.lteixeira.msplanet.application.usecase.AddPlanetUseCase
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+
+@RestController
+class AddPlanetController(val addPlanetUseCase: AddPlanetUseCase) : AddPlanetApi {
+
+    override fun add(@Valid @RequestBody addPlanetRequest: AddPlanetRequest): AddedPlanetResponse {
+        val request = addPlanetRequest.toAddPlanetDomain()
+        return addPlanetUseCase.execute(request).toAddedPlanetResponse()
+    }
+}

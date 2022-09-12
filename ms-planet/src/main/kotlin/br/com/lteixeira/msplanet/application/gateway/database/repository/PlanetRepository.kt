c890b7a196151e94814ac04781ec992247f8d6d3
Lenilson Teixeira <lenilsonts@gmail.com>
2022-09-11 22:33:52 -0300

    [ms-planet] - Adicionando método no repositório para consultar por id externo

--- ms-planet/src/main/kotlin/br/com/lteixeira/msplanet/application/gateway/database/repository/PlanetRepository.kt
+++ ms-planet/src/main/kotlin/br/com/lteixeira/msplanet/application/gateway/database/repository/PlanetRepository.kt
@@ -5,4 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository
 
 interface PlanetRepository : MongoRepository<PlanetDocumet, String> {
     fun findByName(name: String): PlanetDocumet?
+    fun findByExternalId(externalId: String): PlanetDocumet?
 }

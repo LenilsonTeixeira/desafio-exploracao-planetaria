94569c087045a0d8ed8e47e50f767ac2ce02446e
Lenilson Teixeira <lenilsonts@gmail.com>
2022-09-11 22:32:06 -0300

    [ms-planet] - Removendo espaço em branco

--- ms-planet/src/main/kotlin/br/com/lteixeira/msplanet/application/exception/BaseException.kt
+++ ms-planet/src/main/kotlin/br/com/lteixeira/msplanet/application/exception/BaseException.kt
@@ -3,6 +3,5 @@ package br.com.lteixeira.msplanet.application.exception
 import org.springframework.http.HttpStatus
 
 abstract class BaseException(override val message: String): RuntimeException() {
-
     abstract fun getStatus(): HttpStatus
 }

625e800bf496b1eb5beb2d88f890ee88f36b45ee
Lenilson Teixeira <lenilsonts@gmail.com>
2022-09-24 02:45:14 -0300

    [ms-probe] - Ajustando enum Direction

--- ms-probe/src/main/kotlin/br/com/lteixeira/msprobe/application/enumeration/Direction.kt
+++ ms-probe/src/main/kotlin/br/com/lteixeira/msprobe/application/enumeration/Direction.kt
@@ -1,8 +1,15 @@
 package br.com.lteixeira.msprobe.application.enumeration
 
-enum class Direction( name: String) {
-    NORTH("North"),
-    SOUTH("South"),
-    EAST("East"),
-    WEST("West")
+import br.com.lteixeira.msprobe.application.exception.DirectionInvalidException
+
+enum class Direction(val key: String) {
+    NORTH("N"),
+    SOUTH("S"),
+    EAST("E"),
+    WEST("W");
+
+    companion object {
+        fun fromKey(name: String): Direction =
+            Direction.values().find { value -> value.key == name } ?: throw DirectionInvalidException("Direção inválida")
+    }
 }
